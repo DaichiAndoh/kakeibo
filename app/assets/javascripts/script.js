fd30c04cd28485c268fa561f9efcb8042035dc9a
daichi.andoh <daichi.andoh@gmail.com>
2020-01-14 04:36:46 +0000

    Add css, js file

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,15 @@
+/* global $ */
+$(function () {
+    
+    $('.category_group').click(function() {
+    var $category_item = $(this).find('.category_item');
+    if($category_item.hasClass('open')) {
+      $category_item.removeClass('open');
+      $category_item.slideUp();
+    } else {
+      $category_item.addClass('open');
+      $category_item.slideDown();
+    }
+    });   
+    
+});
